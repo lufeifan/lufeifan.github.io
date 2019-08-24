c2ace429afc577109d44e80bbd895780aeefeb34
lufeifan <5244831+lufeifan@user.noreply.gitee.com>
2019-08-24 23:11:47 +0800

    Site updated: 2019-08-24 23:11:45

--- dist/music.js
+++ dist/music.js
@@ -2,14 +2,14 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
     // mini: true,
     audio: [
       {
+        cover: 'http://pwmvr65mz.bkt.clouddn.com/201908231429_860.png',
         name: "PDD洪荒之力",
         artist: '徐梦圆',
         url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
       },
       {
         name: '9420',
